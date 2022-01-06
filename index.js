1a37f134b623d6d32398216f0b053f33a5fa8abe
Conner Ow <connerow1115@gmail.com>
2022-01-06 05:26:13 -0600

    add gitignore

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import { promisify } from 'util';
 import jwt from 'jsonwebtoken';
-//testing local access
 
 import { serialize } from 'cookie';
 
@@ -55,10 +54,10 @@ async function fetchJWT(url, otc) {
   let result;
   let headers = new Headers();
 
-  headers.set('Authorization', 
-        'Basic ' + Buffer.from(process.env.SAASBOX_APP_ID + ":" + process.env.SAASBOX_API_KEY).toString('base64'));
+  headers.set('Authorization',
+    'Basic ' + Buffer.from(process.env.SAASBOX_APP_ID + ":" + process.env.SAASBOX_API_KEY).toString('base64'));
   headers.set('Content-Type', 'application/json');
-  
+
   try {
     // TODO: Add Bearer token here.
     let response = await fetch(JWT_URL, {
@@ -72,19 +71,19 @@ async function fetchJWT(url, otc) {
       // Response ok and JWT
       if (result.jwt) {
         return result.jwt
-      // Response ok but server returned error
+        // Response ok but server returned error
       } else if (result.error) {
         throw new Error(result.error)
-      // Response ok but no JWT, something else we dont want occured
+        // Response ok but no JWT, something else we dont want occured
       } else {
         throw new Error("Fetching User JWT for OTC failed.")
       }
-    // Response not OK
+      // Response not OK
     } else {
       throw new Error("Fetching User JWT for OTC failed.");
     }
-  } catch(err) {
-  	console.error(err);
+  } catch (err) {
+    console.error(err);
     throw new Error(err);
   }
 }
@@ -99,13 +98,13 @@ async function getOTC(req) {
 // Unused, can't set cookie and later use same res to do next(), has to be 
 // all at once.
 async function setCookieJWT(req, res, token) {
-    // Set user auth cookie with JWT
-    return res.cookie('user_auth', token, {
-      httpOnly: true,
-      sameSite: process.env.NODE_ENV == 'production' ? 'none' : 'Lax',
-      secure: process.env.NODE_ENV === 'production' ? true : false,
-      maxAge: 1800000, // 30 minutes
-    });
+  // Set user auth cookie with JWT
+  return res.cookie('user_auth', token, {
+    httpOnly: true,
+    sameSite: process.env.NODE_ENV == 'production' ? 'none' : 'Lax',
+    secure: process.env.NODE_ENV === 'production' ? true : false,
+    maxAge: 1800000, // 30 minutes
+  });
 }
 /* This is not ready yet */
 /*
@@ -114,7 +113,6 @@ const withSboxAuthClient = (handler) => {
       let otc = await getOTC(req)
       let token;
       let user_struct = {};
-
       try {
         if (req.cookies && req.cookies.user_auth) {
           token = req.cookies.user_auth;
@@ -183,90 +181,90 @@ async function validateToken(NextRequest, token) {
     } else {
       throw Error("JWT invalid, sign in again.")
     }
-  } catch(err) {
+  } catch (err) {
     throw new Error(err);
   }
 }
 
-const redirect = function(url) {
-	return {
-  		redirect: {
-  			destination: url
-  		}
-  	}
+const redirect = function (url) {
+  return {
+    redirect: {
+      destination: url
+    }
+  }
 }
 
 // Only for getServerSideProps which gets a ctx argument.
 const withSboxAuth = (handler) => {
-	return async (ctx) => {
-		let req = ctx.req;
-		let res = ctx.res;
-		//console.log("ctx:", ctx);
-		let otc = ctx.query.otc;
-	    let token;
-	    let user_struct = {};
+  return async (ctx) => {
+    let req = ctx.req;
+    let res = ctx.res;
+    //console.log("ctx:", ctx);
+    let otc = ctx.query.otc;
+    let token;
+    let user_struct = {};
 
-	      try {
-	        if (req.cookies && req.cookies.user_auth) {
-	          token = req.cookies.user_auth;
-	          // console.log("Found JWT cookie, validating.")
-	          user_struct = await validateToken(req, token);
-	          // Auth OK, set user
-	          //console.log("Setting valid user")
-	          req.user = user_struct;
-	          // Go to next
-	          return handler(ctx.req, ctx.res);
-	        } else {
-	          //console.log("No JWT cookie, see if OTC is there.")
-	          if (otc) {
-	            // Get JWT
-	            //console.log("OTC is there, fetching JWT with otc=", otc)
-	            token = await fetchJWT(JWT_URL, otc);
-	            if (token) {
-	              // Verify JWT
-	              user_struct = await validateToken(req, token);
-	              // Set Cookie - this doesn't work, cant use returned cookie
-	              // await setCookieJWT(req, res, token);
-	              
-	              // Auth OK, set user:
-	              //console.log("Setting valid user")
-	              req.user = user_struct;
-	              // Go to next
-	              //console.log("Set the cookie with JWT info")
-	              //res.setHeader("Set-Cookie", "foo=bar");
-	              ctx.res.setHeader(
-			          "Set-Cookie",
-			          serialize("user_auth", token, {
-			            // very secure cookies options
-			            httpOnly: true,
-		                sameSite: process.env.NODE_ENV == 'production' ? 'none' : 'Lax',
-		                secure: process.env.NODE_ENV === 'production' ? true : false,
-		                maxAge: process.env.NODE_ENV === 'production' ? 51843000 : 15 // 60 days or 15 seconds
-			          })
-			       );
-	              return handler(ctx.req, ctx.res);
-	              /*return res.cookie("user_auth", token, {
-	                httpOnly: true,
-	                sameSite: process.env.NODE_ENV == 'production' ? 'none' : 'Lax',
-	                secure: process.env.NODE_ENV === 'production' ? true : false,
-	                maxAge: 1800000, // 30 minutes
-	              });*/
-	            }
-	          } else {
-	           // No OTC, No Auth. Redirect to main app for authentication
-	           //console.log("No JWT, no cookie, no OTC, redirecting.")
-	           //return res.redirect(LOGIN_URL);
-	          	return redirect(LOGIN_URL);
-	          }
-	        }
-	      // All errors in try caught here. Ends up in auth redirect.
-	      } catch (error) {
-	        // We could handle this by showing auth_error for 5 seconds
-	        // Then redirecting to saasbox.
-	        // console.log(error);
-	        return handler(ctx.req, ctx.res);
-	      }
-	}
+    try {
+      if (req.cookies && req.cookies.user_auth) {
+        token = req.cookies.user_auth;
+        // console.log("Found JWT cookie, validating.")
+        user_struct = await validateToken(req, token);
+        // Auth OK, set user
+        //console.log("Setting valid user")
+        req.user = user_struct;
+        // Go to next
+        return handler(ctx.req, ctx.res);
+      } else {
+        //console.log("No JWT cookie, see if OTC is there.")
+        if (otc) {
+          // Get JWT
+          //console.log("OTC is there, fetching JWT with otc=", otc)
+          token = await fetchJWT(JWT_URL, otc);
+          if (token) {
+            // Verify JWT
+            user_struct = await validateToken(req, token);
+            // Set Cookie - this doesn't work, cant use returned cookie
+            // await setCookieJWT(req, res, token);
+
+            // Auth OK, set user:
+            //console.log("Setting valid user")
+            req.user = user_struct;
+            // Go to next
+            //console.log("Set the cookie with JWT info")
+            //res.setHeader("Set-Cookie", "foo=bar");
+            ctx.res.setHeader(
+              "Set-Cookie",
+              serialize("user_auth", token, {
+                // very secure cookies options
+                httpOnly: true,
+                sameSite: process.env.NODE_ENV == 'production' ? 'none' : 'Lax',
+                secure: process.env.NODE_ENV === 'production' ? true : false,
+                maxAge: process.env.NODE_ENV === 'production' ? 51843000 : 15 // 60 days or 15 seconds
+              })
+            );
+            return handler(ctx.req, ctx.res);
+            /*return res.cookie("user_auth", token, {
+              httpOnly: true,
+              sameSite: process.env.NODE_ENV == 'production' ? 'none' : 'Lax',
+              secure: process.env.NODE_ENV === 'production' ? true : false,
+              maxAge: 1800000, // 30 minutes
+            });*/
+          }
+        } else {
+          // No OTC, No Auth. Redirect to main app for authentication
+          //console.log("No JWT, no cookie, no OTC, redirecting.")
+          //return res.redirect(LOGIN_URL);
+          return redirect(LOGIN_URL);
+        }
+      }
+      // All errors in try caught here. Ends up in auth redirect.
+    } catch (error) {
+      // We could handle this by showing auth_error for 5 seconds
+      // Then redirecting to saasbox.
+      // console.log(error);
+      return handler(ctx.req, ctx.res);
+    }
+  }
 }
 
 export default withSboxAuth;
