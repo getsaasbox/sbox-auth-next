c19a7d9cef2cfd1ed05d8c2bf485b46f35aae67f
IroncladDev <connerow1115@gmail.com>
2022-03-01 13:27:39 -0600

    Using redis for needsRefresh state.
    Update index.js
    
    A persistent global variable won't work, as if the server scales
    to multiple threads/processes the common variable will be out of
    sync, so we use a tiny bit of memory in a redis instance.
    
    Removed all comments, now we depend on redis

--- index.js
+++ index.js
@@ -2,11 +2,71 @@ import { promisify } from 'util';
 import jwt from 'jsonwebtoken';
 
 import { serialize } from 'cookie';
+import Redis from 'ioredis';
 
 let SAASBOX_DOMAIN = process.env.SAASBOX_DOMAIN ? process.env.SAASBOX_DOMAIN : "http://saasbox.net:8081";
 let LOGIN_URL = SAASBOX_DOMAIN + "/login";
 let JWT_ROUTE = "/api/user-token-otc";
 let JWT_URL = SAASBOX_DOMAIN + JWT_ROUTE;
+// Redis access string, e.g. "rediss://:<values>.upstash.io:32833";
+let SAASBOX_REDIS = process.env.SAASBOX_REDIS
+
+// Saves state that user state is changed and JWT needs refreshing.
+// user state changed webhook from SaaSBox calls this.
+export async function userStateChanged(req, res) {
+  let client = new Redis(SAASBOX_REDIS);
+  if (req.body.id) {
+    await client.set(req.body.id, true);
+  }
+}
+
+// Tells whether the user state has changed and JWT needs refreshing.
+// Call this to re-do token.
+async function hasUserStateChanged(user_struct) {
+  let client = new Redis(SAASBOX_REDIS);
+  let result = await client.get(user_struct.id)
+  if (result == "true") {
+    await client.del(user_struct.id);
+    return true;
+  } else {
+    return false;
+  }
+}
+
+/*
+ * Same logic as in withSboxAuth, somehow refactoring breaks things (should pass handler as arg?)
+ * Fetch new JWT using user id to refresh data structure.
+ * Set cookie with JWT
+ * Return user struct to set req, and call final handler.
+ */
+async function refreshSession(ctx, req, res, otc) {
+  let token;
+  let user_struct;
+
+  try {
+    token = await fetchJWT(JWT_URL, otc);
+    if (token) {
+      // Verify JWT
+      user_struct = await validateToken(req, token);
+      ctx.res.setHeader(
+        "Set-Cookie",
+        serialize("user_auth", token, {
+          // very secure cookies options
+          httpOnly: true,
+          sameSite: process.env.NODE_ENV == 'production' ? 'none' : 'Lax',
+          secure: process.env.NODE_ENV === 'production' ? true : false,
+          maxAge: process.env.NODE_ENV === 'production' ? 51843000 : 60*60 // 1 hour (unit: seconds)
+        })
+      );
+      return user_struct;
+    } else {
+      //throw new Error("Refreshing user token has failed. Token not fetched properly or is invalid.")
+    }
+  } catch (err) {
+    console.error(err);
+    //throw new Error(err);
+  }
+}
 
 // Use as an API handler, the component posts to it from a form to logout.
 export function LogoutApi(req, res) {
@@ -53,11 +113,9 @@ export function init(config) {
 async function fetchJWT(url, otc) {
   let result;
   let headers = new Headers();
-
   headers.set('Authorization',
     'Basic ' + Buffer.from(process.env.SAASBOX_APP_ID + ":" + process.env.SAASBOX_API_KEY).toString('base64'));
   headers.set('Content-Type', 'application/json');
-
   try {
     // TODO: Add Bearer token here.
     let response = await fetch(JWT_URL, {
@@ -65,7 +123,6 @@ async function fetchJWT(url, otc) {
       headers: headers,
       method: 'POST'
     });
-
     if (response.ok) {
       result = await response.json();
       // Response ok and JWT
@@ -106,63 +163,7 @@ async function setCookieJWT(req, res, token) {
     maxAge: 1800000, // 30 minutes
   });
 }
-/* This is not ready yet */
-/*
-const withSboxAuthClient = (handler) => {
-  return async (req, res) => {
-      let otc = await getOTC(req)
-      let token;
-      let user_struct = {};
-      try {
-        if (req.cookies && req.cookies.user_auth) {
-          token = req.cookies.user_auth;
-          // console.log("Found JWT cookie, validating.")
-          user_struct = validateToken(req, token);
-          // Auth OK, set user
-          //console.log("Setting valid user")
-          req.user = user_struct;
-          // Go to next
-          return handler(req, res);
-        } else {
-          //console.log("No JWT cookie, see if OTC is there.")
-          if (otc) {
-            // Get JWT
-            //console.log("OTC is there, fetching JWT with otc=", otc)
-            token = await fetchJWT(JWT_URL, otc);
-            if (token) {
-              // Verify JWT
-              user_struct = await validateToken(req, token);
-              // Set Cookie - this doesn't work, cant use returned cookie
-              // await setCookieJWT(req, res, token);
-              
-              // Auth OK, set user:
-              //console.log("Setting valid user")
-              req.user = user_struct;
-              // Go to next
-              //console.log("Set the cookie with JWT info")
-              return res.cookie("user_auth", token, {
-                httpOnly: true,
-                sameSite: process.env.NODE_ENV == 'production' ? 'none' : 'Lax',
-                secure: process.env.NODE_ENV === 'production' ? true : false,
-                maxAge: 1800000, // 30 minutes
-              });
-            }
-          } else {
-           // No OTC, No Auth. Redirect to main app for authentication
-           //console.log("No JWT, no cookie, no OTC, redirecting.")
-           return res.redirect(LOGIN_URL);
-          }
-        }
-      // All errors in try caught here. Ends up in auth redirect.
-      } catch (error) {
-        // We could handle this by showing auth_error for 5 seconds
-        // Then redirecting to saasbox.
-        //console.log(error);
-        return handler(req, res);
-      }
-  }
-}
-*/
+
 
 /*
  * Returns decoded user from already authenticated JWT cookie, does this for
@@ -176,7 +177,7 @@ async function validateToken(NextRequest, token) {
 
   try {
     if (decoded) {
-      // console.log("Decoded user, TODO: Validate expiry:", decoded);
+       //console.log("Decoded user, TODO: Validate expiry:", decoded);
       return decoded;
     } else {
       throw Error("JWT invalid, sign in again.")
@@ -195,6 +196,7 @@ const redirect = function (url) {
 }
 
 // Only for getServerSideProps which gets a ctx argument.
+// NOTE: if cookie is valid, otc is skipped.
 const withSboxAuth = (handler) => {
   return async (ctx) => {
     let req = ctx.req;
@@ -207,10 +209,15 @@ const withSboxAuth = (handler) => {
     try {
       if (req.cookies && req.cookies.user_auth) {
         token = req.cookies.user_auth;
-        // console.log("Found JWT cookie, validating.")
+        //console.log("Found JWT cookie, validating.")
         user_struct = await validateToken(req, token);
         // Auth OK, set user
         //console.log("Setting valid user")
+        if (await hasUserStateChanged(user_struct)) {
+          // Same user but updated struct.
+          //console.log("Calling refresh session with one in query params, or existing otc in JWT\n")
+          user_struct = await refreshSession(ctx, req, res, otc ? otc : user_struct.otc);
+        }
         req.user = user_struct;
         // Go to next
         return handler(ctx.req, ctx.res);
@@ -227,7 +234,7 @@ const withSboxAuth = (handler) => {
             // await setCookieJWT(req, res, token);
 
             // Auth OK, set user:
-            //console.log("Setting valid user")
+            //console.log("Setting valid user:", user_struct)
             req.user = user_struct;
             // Go to next
             //console.log("Set the cookie with JWT info")
@@ -239,7 +246,7 @@ const withSboxAuth = (handler) => {
                 httpOnly: true,
                 sameSite: process.env.NODE_ENV == 'production' ? 'none' : 'Lax',
                 secure: process.env.NODE_ENV === 'production' ? true : false,
-                maxAge: process.env.NODE_ENV === 'production' ? 51843000 : 15 // 60 days or 15 seconds
+                maxAge: process.env.NODE_ENV === 'production' ? 51843000 : 24*60*60 // 60 days or 1 day
               })
             );
             return handler(ctx.req, ctx.res);
@@ -261,7 +268,8 @@ const withSboxAuth = (handler) => {
     } catch (error) {
       // We could handle this by showing auth_error for 5 seconds
       // Then redirecting to saasbox.
-      // console.log(error);
+      console.log(error);
+      console.log("there was error, calling handler with error.")
       return handler(ctx.req, ctx.res);
     }
   }
